551ed575ec6712f258b98cf4528e55e6414383b0
Gaming32 <soujournme@gmail.com>
2020-09-10 14:41:24 -0500

    added some images

--- loadtop.js
+++ loadtop.js
@@ -18,3 +18,15 @@ fetch("/bottom.html")
     .then(data => {
         document.getElementById("bottom").innerHTML = data;
     });
+
+$('img').each(function() {
+    var deg = $(this).data('rotate') || 0;
+    var rotate = 'rotate(' + deg + 'deg)';
+    $(this).css({ 
+        '-webkit-transform': rotate,
+        '-moz-transform': rotate,
+        '-o-transform': rotate,
+        '-ms-transform': rotate,
+        'transform': rotate 
+    });
+});
